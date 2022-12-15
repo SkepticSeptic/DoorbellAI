19c4e716e5960ba6eb94ddc2c3ee45eb5c966a64
nvidia <SkepticSeptic>
2022-12-15 18:56:05 -0500

    initial commit '

--- doorbellai.py
+++ doorbellai.py
@@ -6,7 +6,7 @@ import time
 # quick definition to take user input for several things such as email, place, and whatever else i decide to use it for:
 def verification(whatis, addmessage=" "): # "oH yOuR VaRiaBLeS aRe sO inConSiStEntLy nAmEd!" it gets worse, trust me
     confirmed = 'not confirmed'
-    while confirmed != 'y' or 'yes': # decided to add yes too for the people that dont know how to read
+    while confirmed != 'y': # decided to add yes too for the people that dont know how to read
         userinput = input("input " + whatis + " " + addmessage)
         confirmed = input("is '"+ userinput + "' your " + whatis + "? (type 'y' & enter to confirm or any other key to try again)")
         if confirmed != 'y':
@@ -17,9 +17,9 @@ def verification(whatis, addmessage=" "): # "oH yOuR VaRiaBLeS aRe sO inConSiStE
 print("Hey lovely user, have you read the readme?\nNo?\nWell what do you think you should do with a file called readme?\n(Hint: The answer might surprise you)\n...\nThats right! You read it! Now go and read it right now, no pressure though totally up to you if you want the script to not work yknow thats on you man i dont really have control over that ya feel?")
 # variables, sender email is a default email. change it if it gets overloaded from people using it (as if)
 bailOut = 'y'
-ownEmail = input("Use default email to send emails from?")
+ownEmail = input("Enter y to use custom sender email (IGNORE UNLESS YOU KNOW WHAT YOURE DOING)")
 print("Unless you've read the 'changing server email' section in the readme and KNOW what youre doing, press any key BESIDES 'y'")
-if ownEmail == 'y' or 'yes' and bailOut == 'y' or 'yes':
+if ownEmail == 'y' and bailOut != 'y':
 	print("Highly reccomended to create/use a burner email for this, as it is the email the SERVER will be using, not the email you will be recieving alerts on.")
 	bailOut = input("Still know what you're doing? Type 'y' to confirm, and anything else to use default server email")
 	ownEmail = input("Enter the SERVER SIDE email to use: ")
@@ -28,13 +28,13 @@ emailPass = 'defaultpassword' # (dont change this variable, its synced to the ac
 # email holding # comment on top of comment here, what the HELL did i mean by this?
 
 # ask for stuff needed for email (recieving email, subject, message, & place)
-userEmail = verification('email', 'to recieve alerts on: ') #email user will recieve alerts on
-subj = verification('subject', 'to title email') #email subject
-message = verification('email message', 'to be alerted by: ') #email message/body text
-place = verification('camera location', '(where is this camera located?)')
+userEmail = verification('email', 'to recieve alerts on:    ') #email user will recieve alerts on
+subj = verification('subject', 'to title email    ') #email subject
+message = verification('email message', 'to be alerted by:    ') #email message/body text
+place = verification('camera location', '(where is this camera located?)    ')
 message = message + '     (camera location:' + place + ')'
 print("Reccomended cooldown time: 600 seconds (10 minutes)")
-cooldown = verification('cooldown', '(how long should the camera wait for after\ndetecting a person? [ENTER IN SECONDS])')
+cooldown = verification('cooldown', '(how long should the camera wait for after\ndetecting a person? [ENTER IN SECONDS])    ')
 
 date='2/22/22' # just messing around here
 
